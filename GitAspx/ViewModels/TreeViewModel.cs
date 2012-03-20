12d0391e7017099487cd2811f615f565496f86e8
Linquize <linquize@yahoo.com.hk>
2012-03-20 23:18:39 +0800

    Add approx repository size

--- GitAspx/ViewModels/TreeViewModel.cs
+++ GitAspx/ViewModels/TreeViewModel.cs
@@ -1,10 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using GitSharp;
 
 namespace GitAspx.ViewModels
 {
     public class TreeViewModel : WebBrowsingBaseViewModel
     {
+        string msRepositorySize;
+
+        public string RepositorySize
+        {
+            get
+            {
+                if (msRepositorySize == null)
+                {
+                    DirectoryInfo loDir = new DirectoryInfo(this.Repository.Directory);
+                    FileInfo[] laFiles = loDir.GetFiles("*", SearchOption.AllDirectories);
+                    long llTotal = laFiles.Sum(a => (a.Length + 1) / 4096) * 4096;
+                    if (llTotal < 1024)
+                        msRepositorySize = llTotal + "bytes";
+                    else if (llTotal < 1024 * 1024)
+                        msRepositorySize = (llTotal / 1024) + "KB";
+                    else if (llTotal < (long)(1024 * 1024 * 1024))
+                        msRepositorySize = (llTotal / (1024 * 1024)) + "MB";
+                    else
+                        msRepositorySize = (llTotal / (1024 * 1024 * 1024)) + "GB";
+                }
+
+                return msRepositorySize;
+            }
+        }
+
         public IEnumerable<Tree> Directories { get; set; }
         public IEnumerable<Leaf> Files { get; set; }
     }
